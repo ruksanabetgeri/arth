dc362b130fa663c0a1252f0c7e2058fcc01e00ce
ruksanabetgeri <ruksanabetageri300@gmail.com>
2025-04-29 11:27:10 +0530

    save

---
+++ calculation.java
@@ -0,0 +1,26 @@
+public class calculation{
+
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 5;
+
+        // Perform addition
+        int sum = num1 + num2;
+        System.out.println("Addition: " + sum);
+
+        // Perform subtraction
+        int difference = num1 - num2;
+        System.out.println("Subtraction: " + difference);
+
+        // Perform multiplication
+        int product = num1 * num2;
+        System.out.println("Multiplication: " + product);
+    }
+}
+
+
+
+
+    
+
